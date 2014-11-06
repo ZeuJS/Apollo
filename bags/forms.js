37cf7d2e1ffa8b5fb0d13ac9aea7bcb2d51c7914
Gaël Demette <demette.gael@gmail.com>
2014-11-06 12:02:24 +0100

    Right package name

--- bags/forms.js
+++ bags/forms.js
@@ -8,5 +8,15 @@ var FormsBag = function FormsBag() {
 
 FormsBag.prototype = Object.create(AbstractBag.prototype);
 FormsBag.prototype.constructor = FormsBag;
+FormsBag.prototype.findById = function findById(search) {
+  var found = AbstractBag.prototype.findById.call(this, search);
+  if (typeof found === 'undefined' || typeof found.form === 'undefined') {
+    throw util.format('Form "%s" not found', search);
+  }
+  return found.form;
+}
+FormsBag.prototype.compile = function compile(search) {
+  var form = FormsBag.prototype.findById.call(this, search);
 
+}
 module.exports = FormsBag;
