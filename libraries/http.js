f5a006192ca904e4bad24f1519497cccdaa04f6c
Gaël Demette <demette.gael@gmail.com>
2016-02-23 19:47:12 +0100

    Header override

--- libraries/http.js
+++ libraries/http.js
@@ -76,18 +76,40 @@ http.ServerResponse.prototype.render = function(templatePath, dataForTemplate, h
     var tpl = swig.compileFile(path);
     httpCode = httpCode || 200;
     dataForTemplate = dataForTemplate || {};
-    var defaultHeader = {
-      'Content-Type': 'text/html; charset=utf-8',
-      'Server': 'ZeuJS/Apollo',
-      'X-Powered-By': 'ZeuJS'
-    };
+    dataForTemplate = MergeRecursive(dataForTemplate, this.renderScope)
+    this.setHeader('Content-Type', 'text/html; charset=utf-8');
+    this.setHeader('Server', 'ZeuJS/Apollo');
+    this.setHeader('X-Powered-By', 'ZeuJS');
     if (typeof header === 'object') {
+      var scopedThis = this;
       Object.keys(header).forEach(function(key) {
-        defaultHeader[key] = header[key];
+        scopedThis.setHeader(key, header[key]);
       });
     }
-    this.writeHead(httpCode, defaultHeader);
+    this.writeHead(httpCode);
     this.end(tpl(dataForTemplate));
 };
 
 module.exports = http;
+
+function MergeRecursive(obj1, obj2) {
+
+  for (var p in obj2) {
+    try {
+      // Property in destination object set; update its value.
+      if ( obj2[p].constructor == Object ) {
+        obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+
+      } else {
+        obj1[p] = obj2[p];
+
+      }
+
+    } catch(e) {
+      // Property in destination object not set; create it and set its value.
+      obj1[p] = obj2[p];
+    }
+  }
+
+  return obj1;
+}
